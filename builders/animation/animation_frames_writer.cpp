4c541ade5eba87336ec9e63e0771156415454fb6
Diego_Cervantes <diego.cervantes5902@alumnos.udg.mx>
2026-08-03 14:18:05 -0600

    fix: Implemented missing frame writing methods Fixed crash by adding missing frame writing methods, porting the frame writing logic from the pre-refactor builder.

--- builders/animation/animation_frames_writer.cpp
+++ builders/animation/animation_frames_writer.cpp
@@ -30,3 +30,111 @@ void AnimationFramesWriter::writeMatrix(
 
     json << "]";
 }
+
+void AnimationFramesWriter::writeSurfaceFrames(
+    std::ostringstream& json,
+    const PlotData& data)
+{
+    json << "\"frames\":[";
+
+    for (size_t i = 0; i < data.frames.size(); i++)
+    {
+        if (i > 0)
+        {
+            json << ",";
+        }
+
+        json << "{";
+
+        json << "\"name\":\"f" << i << "\",";
+        json << "\"traces\":[0],";
+        json << "\"data\":[{";
+        json << "\"z\":";
+
+        writeMatrix(
+            json,
+            data.frames[i]);
+
+        json << "}]";
+        json << "}";
+    }
+
+    json << "]";
+}
+
+void AnimationFramesWriter::writeScatter3DFrames(
+    std::ostringstream& json,
+    const PlotData& data)
+{
+    json << "\"frames\":[";
+
+    for (size_t frameIndex = 0; frameIndex < data.frames.size(); frameIndex++)
+    {
+        if (frameIndex > 0)
+        {
+            json << ",";
+        }
+
+        const auto& frame = data.frames[frameIndex];
+
+        json << "{";
+        json << "\"name\":\"f" << frameIndex << "\",";
+        json << "\"traces\":[0],";
+        json << "\"data\":[{";
+
+        // X
+
+        json << "\"x\":[";
+        for (size_t i = 0; i < frame.size(); i++)
+        {
+            if (i > 0) json << ",";
+            json << frame[i][0];
+        }
+        json << "],";
+
+        // Y
+
+        json << "\"y\":[";
+        for (size_t i = 0; i < frame.size(); i++)
+        {
+            if (i > 0) json << ",";
+            json << frame[i][1];
+        }
+        json << "],";
+
+        // Z
+
+        json << "\"z\":[";
+        for (size_t i = 0; i < frame.size(); i++)
+        {
+            if (i > 0) json << ",";
+            json << frame[i][2];
+        }
+        json << "],";
+
+        // CUSTOM DATA (TWIST)
+
+        json << "\"customdata\":[";
+        for (size_t i = 0; i < frame.size(); i++)
+        {
+            if (i > 0) json << ",";
+            json << frame[i][3];
+        }
+        json << "],";
+
+        // MARKER COLOR (TWIST)
+
+        json << "\"marker\":{\"color\":[";
+        for (size_t i = 0; i < frame.size(); i++)
+        {
+            if (i > 0) json << ",";
+            json << frame[i][3];
+        }
+        json << "]}";
+
+        json << "}]";
+        json << "}";
+    }
+
+    json << "]";
+}
